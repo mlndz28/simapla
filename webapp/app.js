342b622e8ac89b2a49fa9bcd4565687457b016aa
Emilio Rojas <emilio93@gmail.com>
2016-07-25 19:20:58 -0600

    now use const for require vars

--- webapp/app.js
+++ webapp/app.js
@@ -1,14 +1,11 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var database = require('./middleware/mysql/connection');
+const routes = require('./routes/index');
 
 var app = express();
 
@@ -30,13 +27,7 @@ app.use(require('node-sass-middleware')({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function (req, res, next) {
-  database.getConnection();
-  next();
-});
-
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
