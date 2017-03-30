ca906ae3aefd994a4f588906f15c03d90bc28721
Emilio Rojas <emilio93@gmail.com>
2017-03-29 20:04:04 -0600

    add test server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/webPrototype/templates'));
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
