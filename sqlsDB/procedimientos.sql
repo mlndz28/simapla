0ad613892fc3baa8b5fa37ad056aa8e9c9a1e1c5
Marcello Avila <marcelloavila@gmail.com>
2017-08-07 12:51:37 -0600

    Registrar un responsable: funcionando

--- sqlsDB/procedimientos.sql
+++ sqlsDB/procedimientos.sql
@@ -87,6 +87,42 @@ BEGIN
     VALUES(@idPer,1);
 END
 
+Delimiter //
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `registerResponsible`(
+	in `pcarnet` varchar(10),
+	in `pcedula` varchar(12),
+	in `pname` varchar(20),
+	in `plastname` varchar(20),
+	in `pbirthdate` date ,
+	in `ppassword` varchar(45),
+	in `pdateJoinedProgram` date,
+	in `pSpecificAddress` varchar(80),
+	in `pEmail` varchar(255),
+	in `pPhone` varchar(12),
+    in `pIdDistrict` int(11)
+)
+BEGIN
+	INSERT INTO `SimaplaDb`.`Phone`(`number`)VALUES(pPhone);
+    select @pho := idPhone from Phone where number = pPhone;
+
+	INSERT INTO `SimaplaDb`.`Email`(`mail`)VALUES(pEmail);
+    select @mai := idEmail from Email where mail = pEmail;
+
+	INSERT INTO `SimaplaDb`.`SpecificAddress`(`address`, `District_idDistrict`)VALUES(pSpecificAddress, pIdDistrict);
+    select @addr := idSpecificAddress from SpecificAddress where address = pSpecificAddress;
+
+    INSERT INTO `SimaplaDb`.`Person`(`carnet`,`cedula`,`name`,`lastname`,`birthdate`,`password`,`dateJoinedProgram`,`SpecificAddress_idSpecificAddress`,`Email_idEmail`,`Phone_idPhone`)
+	VALUES(pCarnet,pcedula,pname,plastname,pbirthdate,ppassword,pdateJoinedProgram,@addr,@mai,@pho);
+    select @idPer := idPerson, @carnet := carnet as 'carnet' from Person where carnet = pCarnet and Email_idEmail = @mai;
+
+    INSERT INTO `SimaplaDb`.`Responsible`(`Person_idPerson`)
+	VALUES(@idPer);
+
+    INSERT INTO `SimaplaDb`.`UserRoles`(`idPerson`,`role`)
+    VALUES(@idPer,3);
+END
+
 Delimiter &&
 
 
--- webapp/templates/js/app/controllers.js
+++ webapp/templates/js/app/controllers.js
@@ -11,15 +11,15 @@ app
         var url = '/ws/infoStudent';
         /*var params = {
         'action':'getCursos'
-        }*/
-        // Simple GET request example:
-        $http.get(url).then(function successCallback(response) {
-            $scope.me = response.data[0];
-            console.log("LOG controllers.js:"+JSON.stringify($scope.me));
-        }, function errorCallback(response) {
-            console.log("LOG controllers.js: Error.");
-        });
-    }
+    }*/
+    // Simple GET request example:
+    $http.get(url).then(function successCallback(response) {
+        $scope.me = response.data[0];
+        console.log("LOG controllers.js:"+JSON.stringify($scope.me));
+    }, function errorCallback(response) {
+        console.log("LOG controllers.js: Error.");
+    });
+}
 
 /*************
 EXECUTE
@@ -28,70 +28,70 @@ $scope.loadData();
 }])
 
 .controller('SignupCtrl', ['$scope', '$http', function($scope, $http) {
-  $scope.countries = [];
-  $scope.provinces = [];
-  $scope.cantons = [];
-  $scope.districts = [];
-  $scope.instruments = [];
-  $scope.managementPositions = [];
+    $scope.countries = [];
+    $scope.provinces = [];
+    $scope.cantons = [];
+    $scope.districts = [];
+    $scope.instruments = [];
+    $scope.managementPositions = [];
 
-  $scope.entidades = [{
-    value: 0,
-    label: 'Estudiante'
-  },{
-    value: 1,
-    label: 'Administrador'
-  },{
-    value: 2,
-    label: 'Profesor EN DESARROLLO'
-  },{
-    value: 3,
-    label: 'Responsable EN DESARROLLO'
-}]; /*TODO: Procedimientos de registro de profesor y responsable*/
+    $scope.entidades = [{
+        value: 0,
+        label: 'Estudiante'
+    },{
+        value: 1,
+        label: 'Administrador'
+    },{
+        value: 2,
+        label: 'Profesor EN DESARROLLO'
+    },{
+        value: 3,
+        label: 'Responsable'
+    }]; /*TODO: Procedimientos de registro de profesor*/
 
-  $scope.loadCountries = function() {
-    var url = '/ws/countries';
-    $http.get(url).then(function successCallback(response) {
-      $scope.countries = response.data;
-    }, function errorCallback(response) {});
-  };
+    $scope.loadCountries = function() {
+        var url = '/ws/countries';
+        $http.get(url).then(function successCallback(response) {
+            $scope.countries = response.data;
+        }, function errorCallback(response) {});
+    };
 
-  $scope.loadProvinces = function() {
-    var url = '/ws/provinces/1';
-    $http.get(url).then(function successCallback(response) {
-      $scope.provinces = response.data.data;
-    }, function errorCallback(response) {});
-  };
+    $scope.loadProvinces = function() {
+        var url = '/ws/provinces/1';
+        $http.get(url).then(function successCallback(response) {
+            $scope.provinces = response.data.data;
+        }, function errorCallback(response) {});
+    };
 
-  $scope.loadCantons = function() {
-    var url = '/ws/cantons/' + $scope.provincePicker;
-    $http.get(url).then(function successCallback(response) {
-      $scope.cantons = response.data.data;
-    }, function errorCallback(response) {});
-  };
+    $scope.loadCantons = function() {
+        var url = '/ws/cantons/' + $scope.provincePicker;
+        $http.get(url).then(function successCallback(response) {
+            $scope.cantons = response.data.data;
+        }, function errorCallback(response) {});
+    };
 
-  $scope.loadDistricts = function() {
-    var url = '/ws/districts/' + $scope.cantonPicker;
-    $http.get(url).then(function successCallback(response) {
-      $scope.districts = response.data.data;
-    }, function errorCallback(response) {});
-  };
+    $scope.loadDistricts = function() {
+        var url = '/ws/districts/' + $scope.cantonPicker;
+        $http.get(url).then(function successCallback(response) {
+            $scope.districts = response.data.data;
+        }, function errorCallback(response) {});
+    };
 
-  $scope.loadInstruments = function() {
-    var url = '/ws/instruments/';
-    $http.get(url).then(function successCallback(response) {
-      $scope.instruments = response.data.data;
-    }, function errorCallback(response) {});
-  };
+    $scope.loadInstruments = function() {
+        var url = '/ws/instruments/';
+        $http.get(url).then(function successCallback(response) {
+            $scope.instruments = response.data.data;
+        }, function errorCallback(response) {});
+    };
 
-  $scope.loadManagementPositions = function() {
-    var url = '/ws/managementPositions/';
-    $http.get(url).then(function successCallback(response) {
-      $scope.managementPositions = response.data.data;
-    }, function errorCallback(response) {});
-  };
+    $scope.loadManagementPositions = function() {
+        var url = '/ws/managementPositions/';
+        $http.get(url).then(function successCallback(response) {
+            $scope.managementPositions = response.data.data;
+        }, function errorCallback(response) {});
+    };
 
-  $scope.loadManagementPositions();
-  $scope.loadInstruments();
-  $scope.loadProvinces();
+    $scope.loadManagementPositions();
+    $scope.loadInstruments();
+    $scope.loadProvinces();
 }]);
