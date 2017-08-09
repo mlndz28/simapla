112ef3032d2642ebb7cd23ad1aa6cbb92f095a64
Emilio Rojas <emilio93@gmail.com>
2017-08-09 01:04:22 -0600

    add landing page

---
+++ ngApp/src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+import { AngularFontAwesomeModule } from 'angular-font-awesome/angular-font-awesome';
+
+@Component({
+  selector: 'simapla-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
